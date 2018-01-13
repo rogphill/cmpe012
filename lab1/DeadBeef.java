8794ad944b7f8540ee95c8e6972ef303e9b2092d
Rob Phillips <rogphill@ucsc.edu>
2018-01-13 15:53:01 -0800

    Bulk of code done. Still need to write to an output file.

--- lab1/DeadBeef.java
+++ lab1/DeadBeef.java
@@ -1,5 +1,18 @@
 class DeadBeef {
+    static final int RUNS = 1000;
     public static void main(String[] args) {
-        
+        for (int i = 0; i < RUNS; i++) {
+            if (((i+1)%4!=0)&&((i+1)%9!=0)) {
+                System.out.println(i+1);
+            } else if (((i+1)%4==0)&&((i+1)%9==0)) {
+                System.out.println("DEADBEEF");
+            } else if (((i+1)%4==0)) {
+                System.out.println("BEEF");
+            } else if (((i+1)%9==0)) {
+                System.out.println("BEEF");
+            }
+        }
+
     }
+
 }
