26f3d3a2f03d1ba0e3c91061e0a192892c747a99
Rob Phillips <rogphill@ucsc.edu>
2018-01-13 17:59:56 -0800

    Completely working, need to figure out how to generalize path.

--- lab1/DeadBeef.java
+++ lab1/DeadBeef.java
@@ -3,21 +3,22 @@ import java.io.IOException;
 
 class DeadBeef {
     static final int RUNS = 1000;
-    static String fileName = "Output.txt";
+    static String fileName = "C:\\Users\\dacty\\Documents\\UCSC\\CMPE012\\rogphill\\lab1\\Output.txt";
     public static void main(String[] args) {
         try {
             FileWriter output = new FileWriter(fileName);
             for (int i = 0; i < RUNS; i++) {
                 if (((i+1)%4!=0)&&((i+1)%9!=0)) {
-                    output.write(i+1 + "\n");
+                    output.write(i+1 + System.lineSeparator());
                 } else if (((i+1)%4==0)&&((i+1)%9==0)) {
-                    output.write("DEADBEEF\n");
+                    output.write("DEADBEEF" + System.lineSeparator());
                 } else if (((i+1)%4==0)) {
-                    output.write("DEAD\n");
+                    output.write("DEAD" + System.lineSeparator());
                 } else if (((i+1)%9==0)) {
-                    output.write("BEEF\n");
+                    output.write("BEEF" + System.lineSeparator());
                 }
             }
+            output.flush();
             output.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
