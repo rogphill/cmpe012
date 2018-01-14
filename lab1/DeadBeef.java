aa78b49f632c699c77594dbfa5fc18de46dc73f0
Rob Phillips <rogphill@ucsc.edu>
2018-01-13 17:26:09 -0800

    Logic is correct, working on printing to Output.txt

--- lab1/DeadBeef.java
+++ lab1/DeadBeef.java
@@ -1,18 +1,26 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 class DeadBeef {
     static final int RUNS = 1000;
+    static String fileName = "Output.txt";
     public static void main(String[] args) {
-        for (int i = 0; i < RUNS; i++) {
-            if (((i+1)%4!=0)&&((i+1)%9!=0)) {
-                System.out.println(i+1);
-            } else if (((i+1)%4==0)&&((i+1)%9==0)) {
-                System.out.println("DEADBEEF");
-            } else if (((i+1)%4==0)) {
-                System.out.println("BEEF");
-            } else if (((i+1)%9==0)) {
-                System.out.println("BEEF");
+        try {
+            FileWriter output = new FileWriter(fileName);
+            for (int i = 0; i < RUNS; i++) {
+                if (((i+1)%4!=0)&&((i+1)%9!=0)) {
+                    output.write(i+1 + "\n");
+                } else if (((i+1)%4==0)&&((i+1)%9==0)) {
+                    output.write("DEADBEEF\n");
+                } else if (((i+1)%4==0)) {
+                    output.write("DEAD\n");
+                } else if (((i+1)%9==0)) {
+                    output.write("BEEF\n");
+                }
             }
+            output.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
-
     }
-
 }
