751f72134657a893adb5e6459b20f34900433ba5
Rob Phillips <rogphill@ucsc.edu>
2018-02-13 23:24:36 -0800

    Done with program, have to write lab report still.

--- lab4/Lab4.asm
+++ lab4/Lab4.asm
@@ -1,6 +1,7 @@
 # Program File: Lab4.asm
 # Author: Robert George Phillips (rogphill)
 # Email: rogphill@ucsc.edu
+# Description: This program tests a user-inputted number to see if all numbers less than or equal to that number are divisible by 4, divisible by 9, or both. 
 
 .text
 main:
@@ -20,35 +21,51 @@ main:
  	move $s0, $v0
  	
  	# Initializes a counter with user's inputted number, then emulates a for-loop:
+ 	li $t6, 1
  	move $t7, $s0
  	start_loop:
  	
- 		# Checks to see if user's number is divisible by 4:
- 		div $s0, $s1
+ 		# Resets $v0 to ensure proper label jumps in the loop:
+ 		li $v0 0
+ 		
+ 		# Checks to see if number is divisible by 4:
+ 		div $t6, $s1
  		mfhi $t0
  		beqz $t0, dead
  		j checkbeef
  		
  		# Prints "DEAD" if divisible by 4:
  		dead:
-		li $v0, 1
+		li $v0, 4
 		la $a0, outdead
 		syscall
  		
- 		# Checks to see if user's number is divisible by 9:
+ 		# Checks to see if user's is divisible by 9:
  		checkbeef:
- 		div $s0, $s2
+ 		div $t6, $s2
  		mfhi $t0
  		beqz $t0, beef
- 		j increment
+ 		j checknumber
  		
  		# Prints "BEEF" if divisible by 9:
  		beef:
-		li $v0, 1
+		li $v0, 4
 		la $a0, outbeef
 		syscall
+		j increment
 		
+		#If neither DEAD or BEEF, simply prints number:
+		checknumber:
+		beq $v0, $s1, increment
+		li $v0, 1
+		move $a0, $t6
+		syscall
+		
+		# Performs maintenance, such as entering a newline character and incrementing the count:
 		increment:
+		li $v0, 11 # syscall 11 = print character
+    		li $a0, 10 # 10 = newline character
+    		syscall
 		beq $t6, $t7, end_loop
 		addiu $t6, $t6, 1
 		j start_loop
