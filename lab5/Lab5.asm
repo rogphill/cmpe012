b0586ee5230213926536fd11defe83558d7a9496
Rob Phillips <rogphill@ucsc.edu>
2018-02-26 21:20:08 -0800

    Minimum submission requirements.

---
+++ lab5/Lab5.asm
@@ -0,0 +1,11 @@
+# Program File: Lab4.asm
+# Author: Robert George Phillips (rogphill)
+# Email: rogphill@ucsc.edu
+# Description: This program tests a user-inputted number to see if all numbers less than or equal to that number are divisible by 4, divisible by 9, or both. 
+
+.text
+main:
+	
+
+.data
+numberprompt: .asciiz "Please enter a number N: "
